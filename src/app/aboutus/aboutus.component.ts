62ceae38c0336405ff92a811f4f75dbc3e65169c
anasatmani8 <anasatmani8@gmail.com>
2022-11-26 09:59:19 +0100

    Animations Part 2

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,11 +1,21 @@
 import { LeaderService } from './../services/leader.service';
 import { Leader } from './../shared/Leader';
 import { Component, OnInit } from '@angular/core';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.scss']
+  styleUrls: ['./aboutus.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut(),
+      expand()
+    ]
 })
 export class AboutusComponent implements OnInit {
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/Dish';
+import { flyInOut } from '../animations/app.animation';
 
 
 import { DishService } from '../services/dish.service';
@@ -7,7 +8,15 @@ import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut()
+    ]
 })
 
 
